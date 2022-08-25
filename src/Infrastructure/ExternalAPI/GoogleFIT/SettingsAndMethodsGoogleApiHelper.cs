30c5fbee5461ba7482ae6bd7a0da44a0cb942000
Dominik Wikliński <domel2222@gmail.com>
2022-08-25 11:08:00 +0200

    added folder

--- src/Infrastructure/ExternalAPI/GoogleFIT/SettingsAndMethodsGoogleApiHelper.cs
+++ src/Infrastructure/ExternalAPI/GoogleFIT/SettingsAndMethodsGoogleApiHelper.cs
@@ -1,5 +1,4 @@
-﻿
-using System.Globalization;
+﻿using System.Globalization;
 
 namespace Infrastructure.ExternalAPI.GoogleFIT
 {
@@ -7,6 +6,7 @@ namespace Infrastructure.ExternalAPI.GoogleFIT
     {
         public const string clientId = "508951185222-s43tqnlita9f75r07d5295nhjkigih4m.apps.googleusercontent.com";
         public const string clientSecret = "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";
+
         public static string[] scopes = new string[]
             {
                 FitnessService.Scope.FitnessActivityWrite,
@@ -15,6 +15,7 @@ namespace Infrastructure.ExternalAPI.GoogleFIT
                 FitnessService.Scope.FitnessLocationRead,
                 FitnessService.Scope.FitnessBodyRead
             };
+
         public static string ToRfc3339String(this DateTime dateTime)
         {
             return dateTime.ToString("yyyy-MM-dd'T'HH:mm:ss.fffzzz", DateTimeFormatInfo.InvariantInfo);
