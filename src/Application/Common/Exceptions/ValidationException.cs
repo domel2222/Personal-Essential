0b4ca3597f0802805478b0b1f346830289f22755
Dominik Wikliński <domel2222@gmail.com>
2022-10-17 10:03:16 +0200

    added application exception into domain and Validation exception in Application

---
+++ src/Application/Common/Exceptions/ValidationException.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using ApplicationException = Domain.Exceptions.ApplicationException;
+
+namespace Application.Common.Exceptions
+{
+    public class ValidationException : ApplicationException
+    {
+        public IReadOnlyDictionary<string, string[]> ErrorsDictionary { get; }
+        public ValidationException(IReadOnlyDictionary<string, string[]> errorsDictionery)
+            : base("Validation Faliure", "One or more validation errors occured")
+            => ErrorsDictionary = errorsDictionery;
+
+    }
+}
---
+++ src/Domain/Exceptions/ApplicationException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Exeptions
+{
+    public abstract class ApplicationExeption : Exception
+    {
+        public string Title { get; }
+        protected ApplicationExeption(string title, string message) 
+            : base(message) 
+        {
+            Title = title;
+        }
+    }
+}
