402391107b516ff483901a28370289b46baf0632
Dominik Wikliński <domel2222@gmail.com>
2022-10-18 11:17:59 +0200

    create commant for user

---
+++ src/Application/Users/Commands/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,15 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Users.Commands.CreateUser
+{
+    public record CreateUserCommand(string firstName, string lastName, string email) : ICommand<UserResponse>
+    {
+
+    }
+}
