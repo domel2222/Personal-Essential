993d5a021e34b789ce378bf6e8d7000d909e87af
Dominik Wikliński <domel2222@gmail.com>
2022-10-20 10:53:06 +0200

    UserConfiguraion entity

---
+++ src/Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        private Guid guidUser = new Guid("af855ff4-c3e3-4a5e-a5a8-6874bd2f7a31");
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasData(
+                new User
+                {
+                    Id = guidUser,
+                    FirstName = "Dominik",
+                    LastName = "Wikliński",
+                    Email = "domel2222@gmail.com"
+
+                });
+            builder.Property(x => x.FirstName).HasColumnType("nvarchar(100)");
+            builder.Property(x => x.LastName).HasColumnType("nvarchar(100)");
+
+            builder.HasMany(j => j.Journals)
+                    .WithOne(j => j.User)
+                    .HasForeignKey(j => j.UserId);
+
+            builder.HasMany(s => s.SelfAssessmentValues)
+                    .WithOne(s => s.User)
+                    .HasForeignKey(s => s.UserId);
+        }
+    }
+}
