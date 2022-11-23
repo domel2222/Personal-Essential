95bdf3a70963b63dc0b91392202d86c9470ef4db
Dominik Wikliński <domel2222@gmail.com>
2022-11-23 07:30:47 +0100

    test architecture and added assembly references to projects

---
+++ Tests/ArchitectureTests/ArchitectureTests.cs
@@ -0,0 +1,37 @@
+﻿
+namespace ArchitectureTests
+{
+    public class ArchitectureTests
+    {
+        private const string DomainNamespace = "Domain";
+        private const string ApplicationNamespace = "Application";
+        private const string InfrastructureNamespace = "Infrastructure";
+        private const string PresentationNamespace = "Presentation";
+        private const string PersonalManagmentApiNamespace = "PersonalManagment.Api";
+
+        [Fact]
+        public void Domain_Should_Not_HaveDependencyToOtherProjects()
+        {
+            // arrange
+            var assemply = typeof(Domain.AssemblyReference).Assembly;
+
+            var otherProject = new[]
+            {
+                DomainNamespace,
+                ApplicationNamespace,
+                InfrastructureNamespace,
+                PresentationNamespace,
+                PersonalManagmentApiNamespace
+            };
+            // act
+            var result = Types
+                .InAssembly(assemply)
+                .ShouldNot()
+                .HaveDependencyOnAll(otherProject)
+                .GetResult();
+            // assert
+            result.IsSuccessful.Should().BeTrue();
+
+        }
+    }
+}
