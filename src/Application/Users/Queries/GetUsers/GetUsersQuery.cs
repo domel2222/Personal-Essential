8de15fdad08796e755ac9cc219ab60c74d740248
Dominik Wikliński <domel2222@gmail.com>
2022-12-01 09:22:46 +0100

    implemented users queries

---
+++ src/Application/Users/Queries/GetUsers/GetUsersQuery.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Users.Queries.GetUsers
+{
+    public sealed record GetUsersQuery : IQuery<List<UserResponse>>;
+
+}
---
+++ src/Application/Users/Queries/GetUsers/GetUsersQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Users.Queries.GetUsers
+{
+    public sealed class GetUsersQueryHandler : IQueryHandler<GetUsersQuery, List<UserResponse>>
+    {
+        private readonly IUserRepository _userRepository;
+        public GetUsersQueryHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<List<UserResponse>> Handle(GetUsersQuery request, CancellationToken cancellationToken)
+        {
+            var users = await _userRepository.GetAllUserAsync(cancellationToken);
+
+            return users.Adapt<List<UserResponse>>();
+        }
+    }
+}
