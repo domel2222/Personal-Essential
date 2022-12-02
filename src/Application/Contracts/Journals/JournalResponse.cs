3de08ce7cd18736bd1866939b262cfab55b21359
Dominik Wikliński <domel2222@gmail.com>
2022-12-02 12:45:53 +0100

    create journal command and contract

---
+++ src/Application/Contracts/Journals/JournalResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Contracts.Journals
+{
+    public sealed record JournalResponse(Guid id, string Titie, DateTime diaryDate, Guid userId)
+    {
+    }
+}
---
+++ src/Application/Journals/Command/CreateJournal/CreateJournalCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿namespace Application.Journals.Command.CreateJournal
+{
+    public sealed class CreateJournalCommandHandler : ICommandHandler<CreateJournalCommand, JournalResponse>
+    {
+        readonly private IJournalRepository _journalRepository;
+        readonly private IUnitOfWork _unitOfWork;
+        readonly private IMapper _mapper;
+
+        public CreateJournalCommandHandler(IJournalRepository journalRepository, IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _journalRepository = journalRepository;
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<JournalResponse> Handle(CreateJournalCommand request, CancellationToken cancellationToken)
+        {
+            var journal = new Journal
+            {
+                Title = request.Title,
+                Text = request.Text,
+                DiaryDate = request.diaryDate,
+                UserId = request.userId
+            };
+
+            _journalRepository.Insert(journal);
+
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return _mapper.Map<JournalResponse>(journal);
+        }
+    }
+}
