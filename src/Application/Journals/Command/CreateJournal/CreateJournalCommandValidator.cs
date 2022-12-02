c608e6b7ee76f3d3100fc9e9ce8ac6364a705bd0
Dominik Wikliński <domel2222@gmail.com>
2022-12-02 18:22:42 +0100

    added journal validators

--- src/Application/Journals/Command/CreateJournal/CreateJournalCommandValidator.cs
+++ src/Application/Journals/Command/CreateJournal/CreateJournalCommandValidator.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace Application.Journals.Command.CreateJournal
 {
-    internal class CreateJournalCommandValidator
+    public sealed class CreateJournalCommandValidator : AbstractValidator<CreateJournalCommand>
     {
+        public CreateJournalCommandValidator()
+        {
+            RuleFor(x => x.Title)
+                                            .NotEmpty()
+                                            .WithMessage("Title field should not be null or empty")
+                                            .MaximumLength(200);
+
+            RuleFor(x => x.diaryDate).NotEmpty().WithMessage("Diary Date should not be null or empty");
+
+            RuleFor(x => x.diaryDate).Custom((diaryDate, context) =>
+            {
+                if (!CompareDate(diaryDate))
+                {
+                    context.AddFailure("Email :", "Please insert appropriate date. Date should not be greater than actual");
+                }
+            });
+            // check guid 000-0000-000000 
+            RuleFor(x => x.userId).NotEmpty().WithMessage("UsreId should not be null or empty");
+        }
+
+        private bool CompareDate(DateTime incomingDate)
+        {
+            var localTime = DateTime.Now.ToLocalTime();
+            var localDateOnly = new DateOnly(localTime.Year, localTime.Month, localTime.Day);
+            var incomingDateOnly = new DateOnly(incomingDate.Year, incomingDate.Month, incomingDate.Day);
+
+            return (incomingDateOnly <= localDateOnly);
+        }
     }
 }
