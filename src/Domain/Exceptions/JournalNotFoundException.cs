d9b0c2a42d02b4510ee15a445eb4c94f78e93de0
Dominik Wikliński <domel2222@gmail.com>
2022-12-06 10:19:00 +0100

    create journal error

---
+++ src/Domain/Exceptions/JournalNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Exceptions
+{
+    public sealed class JournalNotFoundException : NotFoundException
+    {
+        public JournalNotFoundException(Guid journalId) : base($"The journal with the identifier {journalId} was not found")
+        {
+        }
+    }
+}
